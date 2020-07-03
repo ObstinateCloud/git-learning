4505ded163b33dc535473691065abf5ccf477721
zhangjianyun <zhangjianyun_kd@163.com>
2020-07-03 10:52:43 +0800

    dev分支代码

---
+++ src/com/lengedyun/DevEntity.java
@@ -0,0 +1,10 @@
+package com.lengedyun;
+
+/**
+ * @title: DevEntity
+ * @description: dev分支专有代码
+ * @auther: 张健云
+ * @date: 2020/7/3 10:51
+ */
+public class DevEntity {
+}
--- src/com/lengedyun/Main.java
+++ src/com/lengedyun/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello");
+        System.out.println("Hello Dev");
+        System.out.println("Hello Dev");
     }
 }
